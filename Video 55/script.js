61186ac71d2633d2a64aaede11ba21d3ad6119e4
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-04-14 21:07:15 +0530

    Narendra Singh

--- Video 55/script.js
+++ Video 55/script.js
@@ -1,13 +1,24 @@
 // today we are goin go learn variables,data type & object 
-var a = 9897
+// what is variable - variable is like container which store date for like we store coffie, Tea,  in container similarly varible store data in RAM
+
+
+var a = 9897 // store number
 let b = 93993930
-var c = "Narendra Singh"
+var c = "Narendra Singh" //String
 var _23 = "Hi There GPT THIS side"
 {
-    let a = 10000000001;
+    let s = 10000000001;
     console.log(a)
 
 }
+
+/*There are rules for choosing variable 
+1. letter digit sign allowed for variable Naming
+2. must begin ==> letter, $ and _
+3. javascript reverse not allowd */
+
+
+
 console.log(a+b)
 console.log(typeof a, typeof c, typeof b)
 console.log(_23)
@@ -20,4 +31,19 @@ let x = null
 let y = 394092
 let z = "narendra singh"
 let p = $$
-let 
+let k = true
+console.log(typeof x ,typeof y,typeof z, typeof p, typeof k)
+
+
+//Object 
+
+var nar = {
+    name: "Narendra Singh",
+    "Roll NO": 34
+
+}
+console.log(nar)
+//add more keys 
+nar.salary = "2000cr"
+
+console.log(nar)
--- Video 56/index.js
+++ Video 56/index.js
@@ -1,6 +1,37 @@
-var a = 23;
-var b = 27;
+// /* JavaScript Conditional */
+// var age = prompt("Enter Your Age --> ")
+
+// if(age<18){
+//     console.log("You are not eligible for Voting ")
+// }
+// if(age==18){
+//     console.log("Yes You are now eligible for voting")
+
+// }
+// if (age>18){
+//     console.log("You are eligible for Voting")
+// }
+
+// //Operator's
+
+
+
+// var a = 23;
+// var b = 27;
 
 // console.log(a+b)//for Addition
 // console.log(a%b)//modulus AKA Reminder
-console.log(a-=b)
+// console.log(a-=b)
+
+
+// Logcal operator 
+a = 4;
+b = 8;
+
+if(a==6 || b ==8){
+
+    console.log("Kanti tu galatt hai")
+}
+else{
+    console.log("Mai glat hu")
+}
