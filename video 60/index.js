e2eee0069e226faebaec94e179c1f70f7ed64088
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-04-15 15:16:45 +0530

    Day 01

--- video 60/index.js
+++ video 60/index.js
@@ -1,4 +1,6 @@
 // today we are going to learn about stings 
+// this number from 0 is know as indexing.
+// programmer trolls by saying that your counting starts with 0 
 
 let a = "Narendra"
 console.log(a[0]);
@@ -8,3 +10,28 @@ console.log(a[3]);
 console.log(a[4]);
 
 console.log(a.length)
+
+//Tamplate literals
+
+console.log(`This is real method to deal with kind of statement--> ${a} <-- now a bich me aa gya`)
+
+//escape Sequence 
+
+console.log("hi there i am narendra singh and i am \n coder")
+
+let k = "i am going to print this statement in UPPERCASE here i go"
+console.log(k.toLowerCase())
+
+
+//slice 
+
+let l = "Shivam"
+console.log(l.slice(0,3))
+
+console.log(l.replace("Sh", 77))
+console.log(l.concat(k))
+
+
+let m = "             ndsfjdkslfj          "
+let new_m = m.trim
+console.log(new_m)
