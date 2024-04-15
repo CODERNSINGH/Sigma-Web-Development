f9772b442bd06d130ae9327a001a9f8321f54a67
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-04-15 10:48:26 +0530

    Day 01

--- video 60/index.js
+++ video 60/index.js
@@ -1,1 +1,10 @@
-// Backlog
+// today we are going to learn about stings 
+
+let a = "Narendra"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log(a.length)
