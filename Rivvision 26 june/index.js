c31500b62e4e35796ffd7b35762e5f8f1a53694c
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-06-26 08:56:19 +0530

    Rivision Day

---
+++ Rivvision 26 june/index.js
@@ -0,0 +1,44 @@
+console.log("Everthing is Working Like Butter")
+
+alert("this is alert ")
+
+//variables and all
+
+let a = 34324;
+let b = true;
+let c = "i am Narendra singh";
+var d = "anything";
+const e = 345;
+let g = null;
+
+console.log(typeof a , typeof b, typeof c, typeof g)
+
+{
+    let a = 33434;
+    console.log(a)
+}
+
+//Object 
+let narendra = {
+    name : "narendra singh",
+    "Employee ID" : "23232B",
+    salery : 30000
+}
+
+console.log(narendra)
+
+
+//Conditional 
+
+let Number1 = prompt("Enter Your Age: ")
+
+if (Number1 <18){
+    console.log("You are not eligible for Driving Car")
+}
+else if (Number1 == 18){
+    console.log("Now you can apply for driving ")
+
+}
+else if (18< Number1 == 18){
+    console.log("You can Drive Car")
+}
